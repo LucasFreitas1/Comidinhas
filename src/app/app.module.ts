a82c48f331f6f41c2144246e8ab1d7fd9d1a25ac
Lucas Freitas <luquex_skynet@hotmail.com>
2018-05-03 19:53:06 -0300

    trabalhando com firebase e mudança de páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { InAppBrowser }from '@ionic-native/in-app-browser';
+
 
 import { MyApp } from './app.component';
 import { PrincipalPage } from '../pages/principal/principal';

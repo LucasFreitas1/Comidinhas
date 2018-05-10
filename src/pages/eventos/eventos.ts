bcd4172c17f438fe0bc99ed93bab32d77fb2dc18
UniCeub <uniceub@localhost.localdomain>
2018-05-10 11:02:48 -0300

    Apresentando data em um card no topo

--- src/pages/eventos/eventos.ts
+++ src/pages/eventos/eventos.ts
@@ -20,30 +20,30 @@ export class EventosPage {
   itemsLocal: Observable<any[]>;
   eventosData: any[];
   eventosLocal: any[];
-  eventos:any[];
+  eventos: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, db: AngularFirestore) {
-    
+
     this.itemsData = db.collection('eventos', ref => ref.orderBy('data')).valueChanges();
     this.itemsData.subscribe((cadastrar) => {
       this.eventosData = cadastrar,
-      this.eventos = cadastrar
+        this.eventos = cadastrar
     })
 
     this.itemsLocal = db.collection('eventos', ref => ref.orderBy('localizacao')).valueChanges();
     this.itemsLocal.subscribe((cadastrar) => {
       this.eventosLocal = cadastrar
-      
+
     })
-    
-   
+
+
 
   }
 
-  defineFiltro(valor){
-    if(valor == 1){
+  defineFiltro(valor) {
+    if (valor == 1) {
       this.eventos = this.eventosData;
-    }else if(valor == 2){
+    } else if (valor == 2) {
       this.eventos = this.eventosLocal;
     }
 
@@ -53,7 +53,7 @@ export class EventosPage {
     this.eventos[i].open = !this.eventos[i].open;
   }
 
- 
+
 
 
 
